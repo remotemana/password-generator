01afe052d648a0fc05409b2d1174253a8f340ffb
Lucas Roman <lucas.e.roman@gmail.com>
2022-03-31 23:40:44 -0700

    final tweaks and push before turn in!

--- readme.md
+++ readme.md
@@ -1,1 +1,23 @@
-## Password Generator
+# <Password-Generator>
+
+## Description
+
+Building a random password generator to apply newly aquired javascript concepts. 
+arrays, concatinating, for-loops, 
+
+This project will help me generate strong unique passwords
+
+I learned how to pull a character element out of an array and display is value instead of the index
+
+## Installation 
+
+No install. project is deployed
+
+## Usage 
+
+Generate unique password based upon user input.
+
+
+![mock-up](images/f0e49bcbc623990d71a3b6cd5afdbd79.png)
+
+
--- script.js
+++ script.js
@@ -1,104 +1,117 @@
-//TODO:gererate a password
+  //TODO:gererate a password
 
 
-var passlength = prompt("Choose Length between 8 - 128 characters");
-if (passlength <8) {
-  alert("password length too weak please restart"); 
-} else{}
+  var passlength = prompt("Choose Length between 8 - 128 characters");
+  if (passlength <8) {
+    alert("password length too weak! Please restart")
+  } 
 
+  if (passlength <8) {
+    window.location.reload()
+  }
 
-var numeric = confirm("Include Numeric?");
-
-var uppercase = confirm("Include uppercase?");
-
-var lowercase = confirm("Include Lowercase?");
-
-var special = confirm("Include special characters?");
-
-
-
-
-
-function generatePassword(){
-  //TODO: your code goes here
-
-// var passlength = prompt("Choose Length between 8 - 128 characters");
-
-// var numeric = confirm("Include Numeric?");
-
-// var uppercase = confirm("Include uppercase?");
-
-// var lowercase = confirm("Include Lowercase?");
-
-// var special = confirm("Include special characters?");
-
-//=================================================================================
   
-var numericList = ["0","1","2","3","4","5","6","7","8","9"];
+  if (passlength >128) {
+    alert("password length too long! Please restart")
+  }
 
-var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var specialList = ["?", "@", "#", "$", "&", "%", "*", "^", "+", "=" ];
-
-var userChoice = [];
-
-//================================================================================
-
-// if (passlength <8) {
-//   alert("password length too weak");
-// }
+  if (passlength >128) { 
+    window.location.reload()
+  }
 
 
+  var numeric = confirm("Include Numeric?");
 
-if (numeric === true) {
-  userChoice.push(...numericList);
-}
+  var uppercase = confirm("Include uppercase?");
 
-if (uppercase === true) {
-  userChoice.push(...uppercaseList);
-}
+  var lowercase = confirm("Include Lowercase?");
 
-if(lowercase === true) {
-  userChoice.push(...lowercaseList)
-}
-
-if(special === true) {
-  userChoice.push(...specialList)
-}
-
-var answers = [];
-
-for (let i = 0; i < passlength; i++) {
-  answers.push(userChoice[(Math.floor(Math.random() * userChoice.length))]);
-}
-  return answers.join("");
-};
-
-
-//promt length of characters 8 - 128 
-  //lowercase
-  //uppercase
-  //numeric
-  // and/or special characters
-//when prompts are answered then a password is generated that matches selected criteria 
-
-// display password
+  var special = confirm("Include special characters?");
 
 
 
-// Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  function generatePassword(){
+    //TODO: your code goes here
 
-}
+  // var passlength = prompt("Choose Length between 8 - 128 characters");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // var numeric = confirm("Include Numeric?");
+
+  // var uppercase = confirm("Include uppercase?");
+
+  // var lowercase = confirm("Include Lowercase?");
+
+  // var special = confirm("Include special characters?");
+
+  //=================================================================================
+    
+  var numericList = ["0","1","2","3","4","5","6","7","8","9"];
+
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  var specialList = ["?", "@", "#", "$", "&", "%", "*", "^", "+", "=" ];
+
+  var userChoice = [];
+
+  //================================================================================
+
+  // if (passlength <8) {
+  //   alert("password length too weak");
+  // }
+
+
+
+  if (numeric === true) {
+    userChoice.push(...numericList);
+  }
+
+  if (uppercase === true) {
+    userChoice.push(...uppercaseList);
+  }
+
+  if(lowercase === true) {
+    userChoice.push(...lowercaseList)
+  }
+
+  if(special === true) {
+    userChoice.push(...specialList)
+  }
+
+  var answers = [];
+
+  for (let i = 0; i < passlength; i++) {
+    answers.push(userChoice[(Math.floor(Math.random() * userChoice.length))]);
+  }
+    return answers.join("");
+  };
+
+
+  //promt length of characters 8 - 128 
+    //lowercase
+    //uppercase
+    //numeric
+    // and/or special characters
+  //when prompts are answered then a password is generated that matches selected criteria 
+
+  // display password
+
+
+
+  // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
+  var generateBtn = document.querySelector("#generate");
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
